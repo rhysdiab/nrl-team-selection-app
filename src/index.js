327cee693f038af0357466ad4ac55e3a922e24f8
Rhys Diab <rhys@rhysd-mbp.oneflare.local>
2017-10-31 08:45:18 +1100

    made material design lite and react work together

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'material-design-lite/material.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
