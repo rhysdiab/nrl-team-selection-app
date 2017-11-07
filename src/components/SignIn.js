c563bbbf622940445269a192c8663f5c72e17135
rhys5690 <rhys5690@gmail.com>
2017-11-08 08:31:47 +1100

    added sass manually to webpack config file

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import './SignIn.scss';
 
 const SignIn = ({signIn}) => {
   return (
-    <div>
+    <div className="hello">
       <RaisedButton label="Sign In" onClick={signIn} />
     </div>
   );
