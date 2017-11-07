da9705f77cf5da8864b52cc4fa4b0de1c4b2113f
rhys5690 <rhys5690@gmail.com>
2017-11-08 08:55:09 +1100

    playing with react bootstrap grid system

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import './SignIn.scss';
+import Button from 'react-bootstrap/lib/Button';
 
-const SignIn = ({signIn}) => {
+const SignIn = ({ signIn }) => {
   return (
     <div className="hello">
+      <Row className="show-grid">
+        <Col xs={12} md={8}>
+          <code>
+            &lt;{'Col xs={12} md={8}'} /&gt;
+          </code>
+        </Col>
+        <Col xs={6} md={4}>
+          <code>
+            &lt;{'Col xs={6} md={4}'} /&gt;
+          </code>
+        </Col>
+      </Row>
       <RaisedButton label="Sign In" onClick={signIn} />
     </div>
   );
