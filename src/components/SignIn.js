4ce1bfee917478e082224c8c83c91442f1dae8f7
rhys5690 <rhys5690@gmail.com>
2017-11-09 08:19:01 +1100

    added regular bootstrap to porject and got it working

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import './SignIn.scss';
-import Button from 'react-bootstrap/lib/Button';
 
 const SignIn = ({ signIn }) => {
   return (
     <div className="hello">
-      <Row className="show-grid">
-        <Col xs={12} md={8}>
-          <code>
-            &lt;{'Col xs={12} md={8}'} /&gt;
-          </code>
-        </Col>
-        <Col xs={6} md={4}>
-          <code>
-            &lt;{'Col xs={6} md={4}'} /&gt;
-          </code>
-        </Col>
-      </Row>
-      <RaisedButton label="Sign In" onClick={signIn} />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="my-scss-test">
+              <RaisedButton label="Sign In" onClick={signIn} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
