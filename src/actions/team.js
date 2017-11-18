ce153506f131528a5e1f23e8aeba65ac2c4ae27e
rhys5690 <rhys5690@gmail.com>
2017-11-18 18:15:06 +1100

    created first algorithm attempt

--- src/actions/team.js
+++ src/actions/team.js
@@ -4,16 +4,22 @@ export const updateTeam = (initialTeam, playerMoved, playerDropped) => {
   //algorithm which returns an array which replaces the position of
     //the selected player and pushes every player below the drop source down one
     //target
-  const newTeam = [];
+
   const playerMovedIndex = initialTeam.indexOf(playerMoved);
   const playerDroppedIndex = initialTeam.indexOf(playerDropped);
+
+  const newTeam = [];
 
   //write condition to determine if player being moved up or down
 
   if (playerMovedIndex > playerDroppedIndex) {
     //write algorithm for moving players up
+    for (var i = 0; i < playerDroppedIndex; i++) {
+      const currentPlayer = initialTeam[i];
+      newTeam.push(currentPlayer);
+      return console.log(newTeam);
+    }
 
-    const newArrayUnchanged
 
   } else {
     return console.log('try again');
