5f84a89e7ba260640ad55788d44a3eb47b3582df
rhys5690 <rhys5690@gmail.com>
2017-12-10 07:16:59 +1100

    .

--- src/components/PositionNumber.js
+++ src/components/PositionNumber.js
@@ -18,15 +18,16 @@ function collect(connect, monitor) {
 }
 
 class PositionNumber extends Component {
-  static propTypes = {
-    createNewTeam,
-    connectDropTarget,
-    isOver,
-    player,
-    positionNumber,
-    team,
-    uid
-  };
+  ///add proptypes
+  // static propTypes = {
+  //   createNewTeam,
+  //   connectDropTarget,
+  //   isOver,
+  //   player,
+  //   positionNumber,
+  //   team,
+  //   uid
+  // };
   render() {
     const {
       createNewTeam,
