fb01d950fdadee3d92d67ac69de95fe0137e9da1
rhys5690 <rhys5690@gmail.com>
2017-12-13 08:10:17 +1100

    added facebook sign in button

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { FacebookLogin } from 'react-facebook-login-component';
+import FacebookLogin from 'react-facebook-login';
 import './SignInPage.scss';
 import SignIn from './SignIn';
 
 const SignInPage = ({ signIn }) => {
   return (
     <div className="SignInPage">
-      <SignIn signIn={signIn} />
-      <FacebookLogin socialId="yourAppID"
-                       language="en_US"
-                       scope="public_profile,email"
-                       responseHandler={this.responseFacebook}
-                       xfbml={true}
-                       fields="id,email,name"
-                       version="v2.5"
-                       className="facebook-login"
-                       buttonText="Login With Facebook"/>
+      <div className="SignInPage__wrapper">
+        <div className="SignInPage__wrapper__SignIn">
+          <SignIn signIn={signIn} />
+        </div>
+        <div className="SignInPage__wrapper__FacebookSignIn">
+          <FacebookLogin
+            appId="1088597931155576"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={''}
+            callback={'responseFacebook'}
+          />
+        </div>
+      </div>
     </div>
   );
 };
